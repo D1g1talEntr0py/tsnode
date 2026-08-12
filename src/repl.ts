e2c08847ccb1cd6ebe11e1fd6b4f94c445ad9f10
jason.dimeo <jason.dimeo@gmail.com>
2026-08-12 10:53:37 -0400

    fix(repl): prevent floating promise in REPL eval patch
    - Wraps the async transform logic in a void IIFE inside a synchronous REPLEval function
    - Switches repl.start override to use rest parameters instead of arguments to avoid implicit-eval lint issues
    - Adds JSDoc to the patched eval and start functions

--- src/repl.ts
+++ src/repl.ts
@@ -3,14 +3,23 @@ import repl, { type REPLServer, type REPLEval } from 'node:repl';
 
 const patchEval = (nodeRepl: REPLServer) => {
 	const { eval: defaultEval } = nodeRepl;
-	const preEval: REPLEval = async function(code, context, sourcefile, callback) {
-		const options = {
-			sourcefile, loader: 'ts', tsconfigRaw: { compilerOptions: { preserveValueImports: true } }, define: { require: 'global.require' }
-		} satisfies Parameters<typeof transform>[1];
+	/**
+	 * A custom REPL eval function that transforms TypeScript code to JavaScript before evaluation.
+	 * @param code The TypeScript code to evaluate.
+	 * @param context The context in which the code is evaluated.
+	 * @param sourcefile The source file name for the code being evaluated.
+	 * @param callback The callback function to call with the result of the evaluation.
+	 */
+	const preEval: REPLEval = function(code, context, sourcefile, callback) {
+		void (async () => {
+			const options = {
+				sourcefile, loader: 'ts', tsconfigRaw: { compilerOptions: { preserveValueImports: true } }, define: { require: 'global.require' }
+			} satisfies Parameters<typeof transform>[1];
 
-		try { ({ code } = (await transform(code, options))) } catch {}
+			try { ({ code } = (await transform(code, options))) } catch { /* ignore */ }
 
-		return defaultEval.call(this, code, context, sourcefile, callback);
+			return defaultEval.call(this, code, context, sourcefile, callback);
+		})();
 	};
 
 	// @ts-expect-error overwriting read-only property
@@ -20,6 +29,11 @@ const patchEval = (nodeRepl: REPLServer) => {
 };
 
 const { start } = repl;
-repl.start = function() {
-	return patchEval(Reflect.apply(start, this, arguments));
+/**
+ * A patched version of the Node.js REPL that transforms TypeScript code to JavaScript before evaluation.
+ * @param args The arguments to pass to the Node.js REPL start function.
+ * @returns The patched REPL server instance.
+ */
+repl.start = function(...args) {
+	return patchEval(Reflect.apply(start, this, args));
 };
